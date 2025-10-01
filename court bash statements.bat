c227b4df4bd5844b05a4c54b506f0f4af21fc5a7
SAFIYA-hub-ux <dsafiya03@gmail.com>
2025-10-01 12:27:44 +0530

    Add files via upload

---
+++ court bash statements.bat
@@ -0,0 +1,8 @@
+cd C:\Users\91944\OneDrive\Desktop\court-fetcher-demo
+python --version
+"C:\Users\91944\AppData\Local\Programs\Python\Python313\python.exe" --version
+pip install flask
+>pip install -r requirements.txt
+pip install flask flask-sqlalchemy flask-login
+py -3.13 app.py
+ Running on http://127.0.0.1:5000c
